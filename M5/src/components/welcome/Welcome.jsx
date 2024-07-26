8f596c1daa8aaf11e40c8f9b4ecf684b3dc3316d
bbhgem <149163574+bbhgem@users.noreply.github.com>
2024-07-27 01:54:40 +0200

    Update Welcome.jsx

--- M5/src/components/welcome/Welcome.jsx
+++ M5/src/components/welcome/Welcome.jsx
@@ -4,7 +4,7 @@ import "./welcome.css"
 const Welcome = () => {
   return (
     <>
-    <h1 className="welcome-title text-center mt-4 mb-4">EpiBooks</h1>
+    <h1 className="welcome-title text-center mt-4 mb-4">Bbhgembook</h1>
     <Alert className="alert-bg text-center w-100">
       <p className="m-0">bienvevue chez berfel App.</p>
     </Alert>
